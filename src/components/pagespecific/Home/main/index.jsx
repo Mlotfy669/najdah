c9430df8020d59de34f7d5a34ec0f4a2a3b39418
mlotfy669 <M.lotfy9966@gmail.com>
2022-08-23 20:35:14 +0200

    create info component banner section

--- src/components/pagespecific/Home/main/index.jsx
+++ src/components/pagespecific/Home/main/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import styles from './index.module.scss'
 
 const Main = () => {
+
+  const {t}= useTranslation()
+
   return (
     <div id='main' className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.phoneContainer}>
-          <img src="/assets/images/phone.png" alt="woman image" />
-        </div>
         <div className={styles.info}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint culpa tempore labore dicta et. Voluptas vitae laudantium voluptatibus perferendis temporibus nulla magnam ratione dignissimos nobis quos exercitationem, hic placeat?
+          <h1>{t(`banner.head`)}</h1>
+          <h2>{t(`banner.subhead`)}</h2>
+          <div className={styles.btns}>
+            <button>
+              {t(`banner.bookbtn`)}
+            </button>
+            <button>
+              {t(`banner.appbtn`)}
+            </button>
+          </div>
+        </div>
+        <div className={styles.phoneContainer}>
+          <img src="/assets/images/phone.png" alt="woman" />
         </div>
       </div>
     </div>
-  ) 
+  )
 }
 
 export default Main
