6534975ed4b88e3e87c00d77b3c526d474a0f5c3
mlotfy669 <M.lotfy9966@gmail.com>
2022-08-23 22:21:25 +0200

    create services section

--- src/components/pagespecific/Home/services/index.jsx
+++ src/components/pagespecific/Home/services/index.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import styles from './index.module.scss'
+
 
 const Services = () => {
+  const {t}= useTranslation()
+
+
   return (
-    <div>Services</div>
+    <div id='services' className={styles.container}>
+      <div className={styles.info}>
+        <h2>{t(`services.head`)}</h2>
+        <p>{t(`services.paragraph`)}</p>
+        <p>{t(`services.paragraph`)}</p>
+        <div className={styles.btns}>
+          <button>
+            {t(`banner.bookbtn`)}
+          </button>
+          <button>
+            {t(`banner.appbtn`)}
+          </button>
+        </div>
+      </div>
+      <div className={styles.cards}>
+        <div className={styles.card}>
+          <img src="/assets/images/cardImage1.svg" alt="svg icon" />
+          <span className={styles.title}>{t(`services.cardTitle1`)}</span>
+          <span className={styles.paragraph}>{t(`services.cardParagraph1`)}</span>
+          <button className={styles.more}>{t(`services.learnMore`)}</button>
+        </div>
+        <div className={styles.card}>
+          <img src="/assets/images/cardImage1.svg" alt="svg icon" />
+          <span className={styles.title}>{t(`services.cardTitle1`)}</span>
+          <span className={styles.paragraph}>{t(`services.cardParagraph1`)}</span>
+          <button className={styles.more}>{t(`services.learnMore`)}</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
